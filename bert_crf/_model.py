97ba6242d23425cb491b6a4d888f346db196efdc
jingwenhuang007 <jingwen.huang95@pactera.com>
2019-12-06 14:03:19 -0800

    draft_model

---
+++ bert_crf/_model.py
@@ -0,0 +1,90 @@
+#coding: utf8
+#author: Tian Xia (SummerRainET2008@gmail.com)
+
+from pa_nlp.tf_1x import nlp_tf as TF
+import tensorflow as tf
+from pa_nlp.common import *
+from pa_nlp.tf_1x.bert.bert_model import *
+
+class _Model(object):
+  def __init__(self,
+               max_seq_len,
+               tag_size,  # ['O', 'B_TAG1', 'I_TAG2' ...]
+               is_training,
+               bert_config_file,
+               bert_init_ckpt
+              ):
+
+    self.input_y = tf.placeholder(tf.int32, [None, max_seq_len],
+                                  name="input_y")
+    self.seq_len = tf.placeholder(tf.int32, [None],
+                                  name="seq_len")
+    self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout")
+
+    print(f"name(input_y):{self.input_y.name}")
+    print(f"name(dropout_keep_prob):{self.dropout_keep_prob.name}")
+    
+    self._tag_size = tag_size
+
+    init_probs = tf.get_variable("raw_init_probs", [tag_size], tf.float32)
+    self.init_probs = self._norm(init_probs, 0, "init_probs")
+    
+    trans_probs = tf.get_variable("raw_trans_probs",
+                                  [tag_size, tag_size], tf.float32)
+    self.trans_probs = self._norm(trans_probs, 1, "trans_probs")
+
+    self.bert_model = BertModel(bert_config_file="/Users/jingwenhuang/Documents/NLP/bert/chinese_L-12_H-768_A-12/bert_config.json",
+                                max_seq_length=max_seq_len,
+                                bert_init_ckpt="/Users/jingwenhuang/Documents/NLP/bert/chinese_L-12_H-768_A-12/bert_model.ckpt",
+                                is_training=is_training)
+
+    bert_states = self.bert_model.get_layer_output()[-1]
+    bert_states = tf.unstack(bert_states, axis=1)
+
+    bert_states = tf.unstack(tf.nn.dropout(bert_states,
+                                              self.dropout_keep_prob))
+    self.states2tags = [self._observation_tag_probs(state, pos)
+                        for pos, state in enumerate(bert_states)]
+    
+    self._calc_loss_tf()
+    self._search_tf()
+    
+  def _norm(self, tensor, axis, name):
+    return tf.log(tf.nn.softmax(tensor, axis=axis), name)
+    
+  def _observation_tag_probs(self, bi_LSTM_state, pos):
+    with tf.variable_scope("observation_tag_probs", reuse=pos > 0):
+      return self._norm(tf.layers.dense(bi_LSTM_state, self._tag_size),
+                        1, f"{pos}")
+
+
+  def _calc_loss_tf(self):
+    state_scores = tf.stack(self.states2tags)
+    state_scores = tf.transpose(state_scores, [1, 0, 2])
+    print(f'The size of state_scores is {state_scores.get_shape().as_list()}')
+    log_likelihood, _ = tf.contrib.crf.crf_log_likelihood(inputs=state_scores,
+                                                          tag_indices=self.input_y,
+                                                          transition_params=self.trans_probs,
+                                                          sequence_lengths=self.seq_len
+                                                          )
+    self.loss = tf.identity(tf.reduce_mean(-log_likelihood), name="loss")
+    print(f"name(._loss):{self.loss.name}")
+
+
+  def _search_tf(self):
+    state_scores = tf.stack(self.states2tags)
+    state_scores = tf.transpose(state_scores, [1, 0, 2])
+    opt_seq, opt_seq_prob = tf.contrib.crf.crf_decode(potentials=state_scores,
+                                                      transition_params=self.trans_probs,
+                                                      sequence_length=self.seq_len
+                                                      )
+    self.opt_seq = tf.identity(opt_seq, "opt_seq")
+    self.opt_seq_prob = tf.identity(opt_seq_prob, "opt_seq_prob")
+    print(f"name(opt_seq):{self.opt_seq.name}")
+    print(f"name(opt_seq_prob):{self.opt_seq_prob.name}")
+
+    self.accuracy = tf.identity(
+      TF.accuracy(self.opt_seq, self.input_y), "accuracy"
+    )
+    print(f"name(accuracy):{self.accuracy.name}")
+  
---
+++ bert_crf/data.py
@@ -0,0 +1,100 @@
+#coding: utf8
+#author: Tian Xia (SummerRainET2008@gmail.com)
+
+import os
+from pa_nlp.common import *
+from pa_nlp.tf_1x.bert.bert_tokenizer import *
+'''The format of data is defined as
+Each line is a python dict string, denoting a sample dict.
+{
+text: '...',
+tags = [[pos_from, pos_to, tag_name, source_text] ... ]
+}
+'''
+
+def create_parameter(
+  train_file,
+  vali_file,  # can be None
+  vob_file,
+  tag_list,   # ["person", "organization", ...]
+  max_seq_length=64,
+  epoch_num=1,
+  batch_size=1024,
+  embedding_size=128,
+  RNN_type="lstm",
+  LSTM_layer_num=1,
+  dropout_keep_prob=0.5,
+  learning_rate=0.001,
+  l2_reg_lambda=0.0,
+  evaluate_frequency=100,  # must divided by 100.
+  remove_OOV=True,
+  GPU: int=-1,  # which_GPU_to_run: [0, 4), and -1 denote CPU.
+  model_dir: str= "model",
+  bert_config_file='',
+  bert_init_ckpt='',
+  ):
+  
+  assert os.path.isfile(train_file)
+  assert os.path.isfile(vali_file)
+  
+  tag_list = ["O"] + sorted(set(tag_list) - set("O"))
+  print(f"tag_list: {tag_list}")
+  
+  return {
+    "train_file": os.path.realpath(train_file),
+    "vali_file": os.path.realpath(vali_file),
+    "vob_file": os.path.realpath(vob_file),
+    "tag_list": tag_list,
+    "max_seq_length": max_seq_length,
+    "epoch_num": epoch_num,
+    "batch_size": batch_size,
+    "embedding_size": embedding_size,
+    "RNN_type": RNN_type,
+    "LSTM_layer_num": LSTM_layer_num,
+    "learning_rate": learning_rate,
+    "dropout_keep_prob": dropout_keep_prob,
+    "l2_reg_lambda": l2_reg_lambda,
+    "evaluate_frequency":  evaluate_frequency,
+    "remove_OOV": remove_OOV,
+    "GPU":  GPU,
+    "model_dir": os.path.realpath(model_dir),
+    "bert_config_file": bert_config_file,
+    "bert_init_ckpt": bert_init_ckpt,
+  }
+
+class DataSet:
+  def __init__(self, data_file, tag_list, max_seq_len):
+    self._tag_list = tag_list
+    samples = read_pydict_file(data_file)
+    self.max_seq_len = max_seq_len
+    self._data = [self._gen_label(sample) for sample in samples]
+    self._data_name = os.path.basename(data_file)
+
+
+  def _gen_label(self, sample):
+    word_list = sample["word_list"]
+    original_text = sample["text"]
+    T = BertTokenizer(vocab_file="/Users/jingwenhuang/Documents/NLP/bert/chinese_L-12_H-768_A-12/vocab.txt")
+    tokens, input_ids, input_mask, segment_ids = T.parse_single_sentence(
+      text_a=original_text, max_seq_length=self.max_seq_len
+    )
+    labels = [0] * self.max_seq_len
+    for key, value in sample["tags"].items():
+      for pos in range(value[0], value[1]+1):
+        if pos >= self.max_seq_len:
+          break
+        if pos == value[0]:
+          labels[pos] = self._tag_list.index(key) * 2 - 1
+        else:
+          labels[pos] = labels[value[0]] + 1
+
+    return input_ids[:self.max_seq_len], input_mask[:self.max_seq_len], segment_ids[:self.max_seq_len], labels, min(len(word_list), self.max_seq_len)
+
+
+  def size(self):
+    return len(self._data)
+      
+  def create_batch_iter(self, batch_size, epoch_num, shuffle: bool):
+    return create_batch_iter_helper1(self._data_name, self._data,
+                                    batch_size, epoch_num, shuffle)
+
